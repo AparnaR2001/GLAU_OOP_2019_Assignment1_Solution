8a05d1684a8e656ad5ed9425b62da36d4e91aa96
Flareon19 <aparna.rani_iot18@gla.ac.in>
2019-08-21 16:49:56 +0530

    a. Declare all your necessary variables inside main method. b. Declare a constant for 'gravity', assume the value to be 9.8 c. Calculate the result and print it to the screen.

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -8,16 +8,12 @@ import java.util.Scanner;
 
 public class Gravity {
 	public static void main(java.lang.String[] args) {
-		/* TODO 2:
-		 *   a. Declare all your necessary variables inside main method.
-		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
-		 *   c. Calculate the result and print it to the screen.
-		 * */
-			Scanner scanner = new Scanner(System.in);
-			double time= scanner.nextDouble();
-			double speed=(time*9.8);
-			double distance=(time*9.8*time)/2;
-			System.out.println("The speed of the object at "+time+" seconds after its release is "+speed+" and the distance the object has travelled in the "+time+" seconds after the relase is "+distance);
+        Scanner scanner = new Scanner(System.in);
+        final double gravity = 9.8;
+        double time = scanner.nextDouble();
+        double speed = (time * gravity);
+        double distance = (time * gravity * time) / 2;
+        System.out.println("The speed of the object at " + time + " seconds after its release is " + speed + " and the distance the object has travelled in the " + time + " seconds after the relase is " + distance);
 
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
